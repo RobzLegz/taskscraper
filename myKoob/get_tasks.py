29bfa7fbb5e5946684bf2e5ea723ca51c5af33d5
RobzLegz <robzlegz@gmail.com>
2021-11-23 12:22:15 +0000

    sus

--- myKoob/get_tasks.py
+++ myKoob/get_tasks.py
@@ -3,8 +3,9 @@ import re
 def get_tasks(task_str):
     text = task_str.replace("(", "[")
     text = text.replace(")", "]")
+    print(task_str)
 
-    name = re.findall(r"(?<=\[).+?(?=\])", text)[0]
+    name = re.findall(r"\[(.*?)\]", text)[0]
 
     tasks = task_str.split(f"({name})")
 
